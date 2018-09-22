8019b674955bdd7a0bf33c3df2d07430af00169a
YuriiTrofimenko <tyaa@ukr.net>
2018-09-22 13:30:19 +0300

    add home controller n main-menu view

---
+++ src/main/java/org/tyaa/springmvc1/controller/HomeControoler.java
@@ -0,0 +1,13 @@
+package org.tyaa.springmvc1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeControoler {
+	
+	@RequestMapping("/")
+	public String showPage() {
+		return "main-menu";
+	}
+}
